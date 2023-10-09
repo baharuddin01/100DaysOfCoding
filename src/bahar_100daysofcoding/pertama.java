40fdbd27309def8dee7b4d5ac3a72889df7d33cf
baharuddin <baharuddind40@gmail.com>
2023-10-09 11:40:42 +0800

    Commit Pertama

---
+++ src/bahar_100daysofcoding/pertama.java
@@ -0,0 +1,22 @@
+package bahar_100daysofcoding;
+public class pertama {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data int
+        int umur = 19;
+
+        // Deklarasi variabel dengan tipe data double
+        double tinggi = 175.5;
+
+        // Deklarasi variabel dengan tipe data String
+        String nama = "Baharuddin";
+
+        // Deklarasi variabel dengan tipe data boolean
+        boolean sudahMenikah = false;
+
+        // Menampilkan nilai variabel ke layar
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Sudah Menikah: " + sudahMenikah);
+    }
+}
