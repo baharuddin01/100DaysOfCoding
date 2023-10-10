84cc00ef8332ddb9340332dab03a9884dd520e73
baharuddin01 <baharuddind40@gmail.com>
2023-10-10 18:54:13 +0800

    KeDua

---
+++ src/bahar_100daysofcoding/KeDua.java
@@ -0,0 +1,11 @@
+package bahar_100daysofcoding;
+public class KeDua {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+        
+        int hasilPenjumlahan = angka1 + angka2;
+        
+        System.out.println("Penjumlahan: " + hasilPenjumlahan);
+    }
+}
