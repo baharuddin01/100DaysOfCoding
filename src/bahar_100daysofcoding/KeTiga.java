d1e89c0947040f9186193fc2bb5e139966ddbf55
baharuddin01 <baharuddind40@gmail.com>
2023-10-11 17:15:08 +0800

    KeTiga

---
+++ src/bahar_100daysofcoding/KeTiga.java
@@ -0,0 +1,15 @@
+package bahar_100daysofcoding;
+public class KeTiga {
+    public static void main(String[] args) {
+        // Menentukan panjang dan lebar
+        double panjang = 5.0;
+        double lebar = 3.0;
+        // Menghitung luas dan keliling
+        double luas = panjang * lebar;
+        // Menampilkan hasil
+        System.out.println("Panjang: " + panjang);
+        System.out.println("Lebar: " + lebar);
+        System.out.println("Luas: " + luas);
+    }
+ 
+}
