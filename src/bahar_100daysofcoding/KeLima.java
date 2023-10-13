0105c028ee562c0a693737a9ad48b79de9f5674b
baharuddin01 <baharuddind40@gmail.com>
2023-10-13 21:12:04 +0800

    KeLima

---
+++ src/bahar_100daysofcoding/KeLima.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahar_100daysofcoding;
+public class KeLima {
+    public static void main(String[] args) {
+    byte bilangan1 = 20;
+    byte bilangan2 = 20;
+    
+    byte hasilPenjumlahan = (byte) (bilangan1 + bilangan2);
+    byte hasilPengurangan = (byte) (bilangan1 - bilangan2);
+    byte hasilPembagian = (byte) (bilangan1 / bilangan2);
+    
+    System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+    System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+    System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
