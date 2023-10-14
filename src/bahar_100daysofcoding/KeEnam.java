9eb0af60a207dacb285debb05ff73cc2759a76a3
baharuddin01 <baharuddind40@gmail.com>
2023-10-14 19:33:47 +0800

    KeEnam

---
+++ src/bahar_100daysofcoding/KeEnam.java
@@ -0,0 +1,24 @@
+package bahar_100daysofcoding;
+public class KeEnam {
+    public static void main(String[] args) {
+        
+        byte bilanganByte = 127;
+        short bilanganShort = 32767;
+        int bilanganInt = 2147483647;
+        long bilanganLong = 9223372036854775807L; // Perlu sufiks 'L' atau 'l' untuk menunjukkan tipe long
+        float bilanganFloat = 3.14F; // Perlu sufiks 'F' atau 'f' untuk tipe float
+        double bilanganDouble = 3.14159265359;
+        boolean kondisi = true; // nilai false atau true
+        char karakter = 'A';
+
+        System.out.println("byte: " + bilanganByte);
+        System.out.println("short: " + bilanganShort);
+        System.out.println("int: " + bilanganInt);
+        System.out.println("long: " + bilanganLong);
+        System.out.println("float: " + bilanganFloat);
+        System.out.println("double: " + bilanganDouble);
+        System.out.println("boolean: " + kondisi);
+        System.out.println("char: " + karakter);
+    }
+    
+}
