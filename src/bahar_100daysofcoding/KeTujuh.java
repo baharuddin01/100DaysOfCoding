324a35abd84852f3d0dc5f0a82931d0fce2b2036
baharuddin01 <baharuddind40@gmail.com>
2023-10-15 19:14:56 +0800

    KeTujuh

---
+++ src/bahar_100daysofcoding/KeTujuh.java
@@ -0,0 +1,9 @@
+package bahar_100daysofcoding;
+public class KeTujuh {
+    public static void main(String[] args) {
+        double alas = 6.0;
+        double tinggi = 4.0;
+        double luas = 0.5 * alas * tinggi;
+        System.out.println("Luas Segitiga: " + luas);
+    }
+}
