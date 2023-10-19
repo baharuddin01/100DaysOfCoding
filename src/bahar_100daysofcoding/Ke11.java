dd6397f91f78c2909872edcf87d9c13e0fe5e5d3
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-19 21:49:17 +0800

    ForIfElse

---
+++ src/bahar_100daysofcoding/Ke11.java
@@ -0,0 +1,13 @@
+package bahar_100daysofcoding;
+public class Ke11 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            
+            if(i%2 == 0 ){
+                System.out.println(i+"genap");
+            }else{
+                System.out.println(i+"ganjil");
+            }
+        }
+    }
+}
