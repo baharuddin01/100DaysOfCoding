b19e5ef9796968d3a40e786bb3147062a33384de
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-22 23:44:34 +0800

    perjumlahan

---
+++ src/bahar_100daysofcoding/Ke14.java
@@ -0,0 +1,12 @@
+package bahar_100daysofcoding;
+public class Ke14 {
+    public static void main(String[] args) {
+        int angka1 = 11;
+        int angka2 = 5;
+        
+        System.out.println("Hasil Penambahan : " + angka1 + angka2);
+        System.out.println("Hasil Pengurangan: " + (angka1 - angka2));  // !?....
+        System.out.println("Hasil Perkalian : " + angka1 * angka2);
+        System.out.println("Hasil Pembagian : " + angka1 / angka2);
+    }
+}
