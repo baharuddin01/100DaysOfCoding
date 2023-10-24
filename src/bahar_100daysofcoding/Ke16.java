c8aebfd9ffecd4112a6e4178c718de6f33146cb1
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-24 22:39:51 +0800

    Perulangan

---
+++ src/bahar_100daysofcoding/Ke16.java
@@ -0,0 +1,9 @@
+package bahar_100daysofcoding;
+public class Ke16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Informatika"+i);
+            
+        }
+    }
+}
