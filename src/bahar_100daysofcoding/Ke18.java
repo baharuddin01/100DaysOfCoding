18ba0ca7a0fec73099ca8bbf80c66cf4139a920c
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-26 21:43:47 +0800

    Ke18.java

--- src/bahar_100daysofcoding/Ke18.java
+++ src/bahar_100daysofcoding/Ke18.java
@@ -3,11 +3,11 @@ public class Ke18 {
     public static void main(String[] args) {
         // Outer loop
         for (int i = 1; i <= 3; i++) {
-            System.out.println("Outer: " + i); // Executes 2 times
+            System.out.println("Outer: " + i); 
   
         // Inner loop
             for (int j = 1; j <= 5; j++) {
-                System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+                System.out.println(" Inner: " + j); 
             }
         } 
     }
