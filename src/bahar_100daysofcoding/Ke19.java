414e8490311f64c2eef3c293c1a37c2f3179c1bb
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-27 22:29:27 +0800

    Penggunaan loop for-each

---
+++ src/bahar_100daysofcoding/Ke19.java
@@ -0,0 +1,15 @@
+package bahar_100daysofcoding;
+public class Ke19 {
+    public static void main(String[] args) { 
+        String info = "nama nama merak HP  :" ;
+        System.out.println(info);
+        String[] cars = {
+            "      vivo",
+            "      samsung" , 
+            "      oppo" , 
+            "      nokia"};
+        for (String i : cars ) {
+            System.out.println(i);
+        }
+    }
+}
