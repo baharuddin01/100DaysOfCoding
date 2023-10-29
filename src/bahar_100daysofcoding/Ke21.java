f21c5dbe00064b49bb7a154c6abde272a0528bf7
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-10-29 22:43:26 +0800

    penggunaan boolean

---
+++ src/bahar_100daysofcoding/Ke21.java
@@ -0,0 +1,9 @@
+package bahar_100daysofcoding;
+public class Ke21 {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 31;
+        boolean banding = (x > y) ; 
+        System.out.println(banding);
+    }
+}
