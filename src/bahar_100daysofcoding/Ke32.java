302771a776818774b6b6d54297f5d6a5a5fbdc4e
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-09 22:43:37 +0800

    Array 1D

---
+++ src/bahar_100daysofcoding/Ke32.java
@@ -0,0 +1,14 @@
+package bahar_100daysofcoding;
+public class Ke32 {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi array
+        String data[] = {"Bahar", "Saldi", "Masdi" ,"Ikbal"};
+        
+        // Loop untuk menampilkan isi array beserta indeksnya
+        for (int i = 0 ; i < data.length; i++) 
+        {
+            System.out.println("Index ke : " + i +" = "+ data[i]);
+        }
+        
+    }
+}
