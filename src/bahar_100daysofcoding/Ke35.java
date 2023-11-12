6e75d461113fa2330ef115d704865955b999707a
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-12 23:17:12 +0800

    mengubah nama depan

---
+++ src/bahar_100daysofcoding/Ke35.java
@@ -0,0 +1,16 @@
+package bahar_100daysofcoding;
+public class Ke35 {
+    public static void main(String[] args) {
+    // Mendefinisikan variabel namaDepan dan namaBelakang
+       String namaDepan = "Muhammad"; 
+       String namaBelakang = "Bahar"; 
+
+       System.out.println("Nama lengkap: " + namaDepan + " " + namaBelakang);
+
+    // Mengubah nama depan menjadi "Abdul"
+       namaDepan = "Abdul";
+
+    // Menampilkan nama lengkap setelah mengubah nama depan
+       System.out.println("Nama lengkap setelah diubah: " + namaDepan + " " + namaBelakang);
+    }
+}
