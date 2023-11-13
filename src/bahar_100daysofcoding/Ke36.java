dec420763610cc401fccb15c18493c45040c7faf
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-13 23:59:15 +0800

    array 1D dan loop for

---
+++ src/bahar_100daysofcoding/Ke36.java
@@ -0,0 +1,13 @@
+package bahar_100daysofcoding;
+public class Ke36 {
+    public static void main(String[] args) {
+        // Mendefinisikan sebuah array int dengan 5 elemen
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Menampilkan isi dari array
+        System.out.println("Isi dari array numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element ke-" + i + ": " + numbers[i]);
+        }
+    }
+}
