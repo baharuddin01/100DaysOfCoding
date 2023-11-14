d3ef055fac32cc36d4dd0149c7b6e4811c3ea8da
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-14 22:17:12 +0800

    if-else

--- src/bahar_100daysofcoding/Ke37.java
+++ src/bahar_100daysofcoding/Ke37.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bahar_100daysofcoding;
 
-/**
- *
- * @author masdi
- */
+import java.util.Scanner;
+
 public class Ke37 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("nama :");
+        String nama = sc.nextLine();
+        
+        System.out.print("umur :");
+        int umur = sc.nextInt();
+        
+        if (17 > umur ) {
+            System.out.println("blum bisa membuat sim");
+        }else{
+            System.out.println("sudah bisa membuat sim");
+        }
+    }
 }
