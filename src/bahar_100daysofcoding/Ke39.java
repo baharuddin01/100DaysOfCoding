e32f5eb072fb25c49b4f2f81710ff918c4d2e40b
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-16 20:52:01 +0800

    array

---
+++ src/bahar_100daysofcoding/Ke39.java
@@ -0,0 +1,22 @@
+package bahar_100daysofcoding;
+public class Ke39 {
+    public static void main(String[] args) {
+        int [] array;
+    
+        array = new int [12];
+        array[0] = 31;
+        array[1] = 29;
+        array[2] = 31;
+        array[3] = 30;
+        array[4] = 31;
+        array[5] = 30;
+        array[6] = 31;
+        array[7] = 30;
+        array[8] = 31;
+        array[9] = 30;
+        array[10] = 30;
+        array[11] = 31;
+        
+        System.out.println("menampilkan ke 2 : "+array[3]);
+    }
+}
