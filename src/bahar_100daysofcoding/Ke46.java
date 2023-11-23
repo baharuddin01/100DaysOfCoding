468720e52cd65a72c71780296b97cd0df585c05a
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-23 23:25:56 +0800

    pola persegi

--- src/bahar_100daysofcoding/Ke46.java
+++ src/bahar_100daysofcoding/Ke46.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bahar_100daysofcoding;
 public class Ke46 {
     public static void main(String[] args) {
-        int tinggi = 6; // Anda dapat mengganti tinggi sesuai keinginan
-        int lebar = 4; // Anda dapat mengganti lebar sesuai keinginan
+        int tinggi = 6;
+        int lebar = 4; 
 
         // Membuat pola persegi panjang bintang
         for (int i = 1; i <= tinggi; i++) {
