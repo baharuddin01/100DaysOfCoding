87a080f85df14eba4e37627707ca68b1d455d623
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-11-29 22:00:03 +0800

    ArrayInt dan ArrayString

---
+++ src/bahar_100daysofcoding/Ke52.java
@@ -0,0 +1,45 @@
+package bahar_100daysofcoding;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ke52 {
+    public static void main(String[] args) {
+        // Membuat array untuk menyimpan string dan angka
+        ArrayList<String> arrayString = new ArrayList<>();
+        ArrayList<Integer> arrayAngka = new ArrayList<>();
+
+        // Membuat objek Scanner untuk membaca input dari pengguna
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah karakternya: ");
+        int jumlah = sc.nextInt();
+
+        // Membersihkan newline character di buffer
+        sc.nextLine();
+
+        for (int i = 1; i <= jumlah; i++) {
+            System.out.print("Masukkan input ke-" + i + ": ");
+            String input = sc.nextLine();
+
+            // Memeriksa apakah input merupakan string atau angka
+            try {
+                // Jika dapat diubah menjadi angka, tambahkan ke dalam array angka
+                int angka = Integer.parseInt(input);
+                arrayAngka.add(angka);
+            } catch (NumberFormatException e) {
+                // Jika tidak dapat diubah menjadi angka, tambahkan ke dalam array string
+                arrayString.add(input);
+            }
+        }
+
+        // Menampilkan isi array string
+        System.out.println("Array String: " + arrayString);
+
+        // Menampilkan isi array angka
+        System.out.println("Array Angka: " + arrayAngka);
+
+        // Menutup scanner
+        sc.close();
+    }
+}
