237bcb3b634f4b1b4a08e8a5592cd84a03a598a4
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-12-09 01:10:41 +0800

    Add files via upload

---
+++ src/bahar_100daysofcoding/Ke61.java
@@ -0,0 +1,11 @@
+package bahar_100daysofcoding;
+public class Ke61 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("(" + i + "," + j + ") ");
+            }
+            System.out.println();
+        }
+    }
+}
