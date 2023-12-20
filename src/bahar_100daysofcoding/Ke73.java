4b46abd320a8c8520d4a36b58b0c944bd1370c4d
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-12-20 23:43:49 +0800

    ganjil dan genap

---
+++ src/bahar_100daysofcoding/Ke73.java
@@ -0,0 +1,16 @@
+package bahar_100daysofcoding;
+public class Ke73 {
+    public static void main(String[] args) {
+        System.out.println("bilangan ganil : ");
+        for (int i = 1; i < 10; i+=2) { 
+            System.out.print("");
+            System.out.println(i);
+            
+        }
+        System.out.println("bilangan genap : ");
+        for (int j = 0; j < 10; j+=2) {
+            System.out.print("");
+            System.out.println(j);
+        }
+    }
+}
