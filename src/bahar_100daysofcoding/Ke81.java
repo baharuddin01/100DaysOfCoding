e532524a82328bfdd186505ce33b3f8563c71ecb
baharuddin01 <130576948+baharuddin01@users.noreply.github.com>
2023-12-29 21:58:51 +0800

    jumlah bilangan prima

---
+++ src/bahar_100daysofcoding/Ke81.java
@@ -0,0 +1,37 @@
+package bahar_100daysofcoding;
+public class Ke81 {
+
+    public static void main(String[] args) {
+        // Tentukan rentang bilangan
+        int batasBawah = 1;
+        int batasAtas = 20;
+
+        // Hitung jumlah bilangan prima dalam rentang
+        int jumlahPrima = hitungJumlahPrima(batasBawah, batasAtas);
+
+        // Tampilkan hasil
+        System.out.println("Jumlah bilangan prima antara " + batasBawah + " dan " + batasAtas + " adalah " + jumlahPrima);
+    }
+
+    private static int hitungJumlahPrima(int batasBawah, int batasAtas) {
+        int jumlahPrima = 0;
+        for (int i = batasBawah; i <= batasAtas; i++) {
+            if (isPrima(i)) {
+                jumlahPrima++;
+            }
+        }
+        return jumlahPrima;
+    }
+
+    private static boolean isPrima(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
